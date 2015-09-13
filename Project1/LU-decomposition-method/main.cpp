f592a8de410c2a14ff3cffb94ed725e558c63939
Henriklimseth <henrikslimseth@gmail.com>
2015-09-13 02:23:39 +0200

    Update main.cpp

--- Project1/LU-decomposition-method/main.cpp
+++ Project1/LU-decomposition-method/main.cpp
@@ -10,6 +10,8 @@ using namespace arma;
 
 using namespace std;
 
+// Function for computing right hand side values of the differential equation. Takes vector of x-values
+// and returns vector of function values.
 vec RHS(vec x){
    int n = x.n_rows;
    int i;
@@ -20,6 +22,7 @@ vec RHS(vec x){
    return values;
 }
 
+// The analytical solution of the DE. Takes vector of x-values and returns vector of function values.
 vec analytic(vec y){
     int n = y.n_rows;
     vec values(n);
@@ -29,6 +32,9 @@ vec analytic(vec y){
     }
     return values;
 }
+
+// Function for computing log10(|relative error|). Call: relative_error(numerical solution vector, analytical solution vector)
+// and returns vector of log10(|relative error|).
 
 vec relative_error(vec u, vec y){
 
@@ -43,7 +49,7 @@ vec relative_error(vec u, vec y){
 
         error(i) = log10(fabs((u(i)-y(i))/y(i)));
 
-        cout << error(i) << endl;
+        
 
     }
     return error;
@@ -65,7 +71,8 @@ int main()
 
     int i;
     int j;
-
+    
+// Setting up the tridiagonal matrix A:
     for (i=0;i<n;i++){
         for(j=0;j<n;j++){
             if (i==j){
@@ -81,21 +88,36 @@ int main()
         x(i) = (1+i)*h;
 
     }
+    
+    // Setting up the timer:
+    clock_t start, finish;
+    start = clock();
+    
+    // Run algorithm many times to get accurate time measurement
+    int k = 100;     // Remember to change k relative to n
+    int t;
+    for (t=0; t<k; t++){
+    
+    // LU decomposition
     mat L, U, P;
     lu(L,U,P,A);
 
+// The outfile will contain one single column with first the x-values, then the numerical solution, then the relative error,
+// all separated by one blank line each. See cunning python plot program for how to handle this ouput.
     ofstream outfile;
     string file_name = "ludecomp_values_n" + to_string(n) + ".txt";
     outfile.open(file_name);
 
-    vec f(n);
+    vec f(n);        // The right hand side
     f = RHS(x);
 
-    vec u(n);
+    vec u(n);        // Numerical solution
 
-    vec analsol(n);
+    vec analsol(n);        // Analytical solution
     analsol = analytic(x);
 
+
+// Solving the set of equations using the LU-decomposition:
     vec y(n);
     y = solve(L,f);
     u = solve(U,y);
@@ -106,7 +128,9 @@ int main()
     outfile << x << "   " << endl;
     outfile << u << "   " << endl;
     outfile << log10error;
-
+}
+finish = clock();
+cout <<( (float)(finish-start)/CLOCKS_PER_SEC/k )<< endl;
 
     return 0;
 }
